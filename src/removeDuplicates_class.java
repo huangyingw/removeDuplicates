6787651712b08a2644951d721ed70364dafb7f2b
huangyingw@linux_programming <huangyingw@gmail.com>
2012-02-03 23:38:59 +0800

    n

--- src/removeDuplicates_class.java
+++ src/removeDuplicates_class.java
@@ -1,23 +1,24 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class removeDuplicates_class {
 	static void removeDuplicates(List<String> obj) {
-		for (int i = 0; i < obj.size(); i++) {
-			for (int j = obj.size() - 1; j > i; j--) {
-				if (obj.get(i).equals(obj.get(j))) {
-					obj.remove(j);
-				}
-			}
-		}
+		Set<String> set = new HashSet<String>();
+		set.addAll(obj);
+		obj.clear();
+		obj.addAll(set);
 	}
 
 	public static void main(String args[]) {
 		// String[] arr = { "good", "better", "best", "best", "first", "last",
 		// "last", "last", "good" };
 		// List test = Arrays.asList(arr);
-		List<String> test1 = Arrays.asList("good", "better", "best", "best",
-				"first", "last", "last", "last", "good");
+		List<String> test1 = new ArrayList<String>(Arrays.asList("good",
+				"better", "best", "best", "first", "last", "last", "last",
+				"good"));
 		removeDuplicates(test1);
 		for (String str : test1) {
 			System.out.print(str + ",");
