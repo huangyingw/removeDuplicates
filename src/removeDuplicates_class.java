66c03bce0d3bd8eb2ef1d75ba8491ac1ae6e16d9
huangyingw@linux_programming <huangyingw@gmail.com>
2012-02-03 21:35:41 +0800

    n

---
+++ src/removeDuplicates_class.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class removeDuplicates_class {
+	static void removeDuplicates(List obj) {
+		// equals
+	}
+
+	public static void main(String args[]) {
+		String[] arr = { "good", "better", "best", "best", "first", "last",
+				"last", "last", "good" };
+		List test = Arrays.asList(arr);
+		List test1 = Arrays.asList("good", "better", "best", "best", "first",
+				"last", "last", "last", "good");
+
+	}
+}
