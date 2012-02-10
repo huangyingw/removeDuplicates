3c3c8fec60c956c8cd23bd0dd27b229396be4353
huangyingw@linux_programming <huangyingw@gmail.com>
2012-02-10 10:06:21 +0800

    n

--- src/removeDuplicates_class.java
+++ src/removeDuplicates_class.java
@@ -5,25 +5,6 @@ import java.util.List;
 import java.util.Set;
 
 public class removeDuplicates_class {
-	static void removeDuplicates(List<String> obj) {
-		if (obj == null)
-			return;
-		for (int i = 0; i < obj.size(); i++) {
-			for (int j = obj.size() - 1; j > i; j--) {
-				if (obj.get(i).equals(obj.get(j))) {
-					obj.remove(j);
-				}
-			}
-		}
-	}
-
-	static void removeDuplicates_set(List<String> obj) {
-		Set<String> set = new HashSet<String>();
-		set.addAll(obj);
-		obj.clear();
-		obj.addAll(set);
-	}
-
 	public static void main(String args[]) {
 		List<String> test1 = new ArrayList<String>(Arrays.asList("good",
 				"better", "best", "best", "first", "last", "last", "last",
@@ -41,5 +22,24 @@ public class removeDuplicates_class {
 			System.out.print(str + ",");
 		}
 
+	}
+
+	static void removeDuplicates(List<String> obj) {
+		if (obj == null)
+			return;
+		for (int i = 0; i < obj.size(); i++) {
+			for (int j = obj.size() - 1; j > i; j--) {
+				if (obj.get(i).equals(obj.get(j))) {
+					obj.remove(j);
+				}
+			}
+		}
+	}
+
+	static void removeDuplicates_set(List<String> obj) {
+		Set<String> set = new HashSet<String>();
+		set.addAll(obj);
+		obj.clear();
+		obj.addAll(set);
 	}
 }
